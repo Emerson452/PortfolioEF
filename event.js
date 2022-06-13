ab88b155ed37f294158154a6bbf5db240965c24a
Emerson452 <emerson452pro@gmail.com>
2022-06-13 09:00:24 +0200

    portfolio

---
+++ event.js
@@ -0,0 +1,17 @@
+function metier1() {
+    var div = document.getElementById("list_metier1");
+    if (div.style.display === "none") {
+    div.style.display = "block";
+    } else {
+    div.style.display = "none";
+    }
+    }
+
+function metier3() {
+    var div = document.getElementById("list_metier2");
+    if (div.style.display === "none") {
+    div.style.display = "block";
+    } else {
+    div.style.display = "none";
+    }
+    }
